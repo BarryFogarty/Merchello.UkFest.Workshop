40b145095ff9ec3c014862b10e48a7889c4e8ce0
Barry Fogarty <code@barryfogarty.com>
2015-10-19 19:38:50 +0100

    Removes UmbracoProperties attribute on MetaValues

--- Merchello.UkFest.Web/Models/MetaValues.cs
+++ Merchello.UkFest.Web/Models/MetaValues.cs
@@ -7,19 +7,20 @@ namespace Merchello.UkFest.Web.Models
     /// <summary>
     /// The meta values.
     /// </summary>
-    [UmbracoProperties(Prefix = "Head", Recursive = true)]
+    //[UmbracoProperties(Prefix = "Head")]
     public class MetaValues
     {
         /// <summary>
         /// Gets or sets the page title.
         /// </summary>
-        //[UmbracoProperty("HeadTitle", "Name")]
+        [UmbracoProperty("HeadTitle", "Name")]
         public string Title { get; set; }
 
         /// <summary>
         /// Gets or sets the Meta description.
         /// </summary>
         [TypeConverter(typeof(MetaDescriptionConverter))]
+        [UmbracoProperty("HeadDescription", "Name")]
         public string Description { get; set; }
     }
 }
