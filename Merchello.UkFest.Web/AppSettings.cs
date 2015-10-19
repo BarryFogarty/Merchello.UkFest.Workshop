f7692fea8499ceaa6fb13acdc1666a025e436aff
Barry Fogarty <code@barryfogarty.com>
2015-10-19 17:11:21 +0100

    Uses a ditto picker converter to return product image collections Utility functions for getting crops Moves crop call into product partials Adjusts design of product detail page Removes duplicate category header call

---
+++ Merchello.UkFest.Web/AppSettings.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+
+namespace Merchello.UkFest.Web
+{
+    public static class AppSettings
+    {
+        #region App Settings
+
+        public static object CropBackgroundColour
+        {
+            get { return Setting<string>("CropBackgroundColour"); }
+        }
+
+
+        #endregion
+
+        #region Generic getter
+
+        private static T Setting<T>(string name)
+        {
+            string value = ConfigurationManager.AppSettings[name];
+
+            if (value == null)
+            {
+                throw new Exception(String.Format("Could not find setting '{0}',", name));
+            }
+
+            return (T)Convert.ChangeType(value, typeof(T), CultureInfo.InvariantCulture);
+        }
+
+        #endregion
+    }
+}
---
+++ Merchello.UkFest.Web/Models/Image.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Our.Umbraco.Ditto;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.PublishedContent;
+using umbConstants = Umbraco.Core.Constants.Conventions;
+
+namespace Merchello.UkFest.Web.Models
+{
+    [TypeConverter(typeof(DittoPickerConverter))]
+    public class Image : PublishedContentModel
+    {
+        public Image(IPublishedContent content)
+            : base(content)
+        {
+        }
+
+        [UmbracoProperty(umbConstants.Media.Bytes)]
+        public int Bytes { get; set; }
+
+        [UmbracoProperty(umbConstants.Media.Width)]
+        public int Width { get; set; }
+
+        [UmbracoProperty(umbConstants.Media.Height)]
+        public int Height { get; set; }
+
+        [UmbracoProperty(umbConstants.Media.Extension)]
+        public string Extension { get; set; }
+    }
+}
