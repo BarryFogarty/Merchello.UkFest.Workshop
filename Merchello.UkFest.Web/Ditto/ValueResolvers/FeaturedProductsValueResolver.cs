b9831a5fecde380e86d31e573d7a99c8db292cff
Barry Fogarty <code@barryfogarty.com>
2015-10-20 12:36:51 +0100

    Implements featured products ( product collection picker on home)

---
+++ Merchello.UkFest.Web/Ditto/ValueResolvers/FeaturedProductsValueResolver.cs
@@ -0,0 +1,41 @@
+﻿using Merchello.UkFest.Web.Resolvers;
+
+namespace Merchello.UkFest.Web.Ditto.ValueResolvers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using Merchello.UkFest.Web.Models;
+    using Merchello.Web.Models.VirtualContent;
+
+    using Our.Umbraco.Ditto;
+
+    using Umbraco.Web;
+
+    /// <summary>
+    /// A resolver for product listings.
+    /// </summary>
+    public class FeaturedProductsValueResolver : DittoValueResolver
+    {
+        /// <summary>
+        /// Resolves the <see cref="ProductListing"/> from Merchello's <see cref="IEnumerable{IProductContent}"/>.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="object"/>.
+        /// </returns>
+        public override object ResolveValue()
+        {
+            var empty = Enumerable.Empty<ProductListItem>();
+
+            if (Content == null) return empty;
+
+            var root = ContentResolver.Instance.GetRootContent();
+
+            var featuredProducts = root.GetPropertyValue<IEnumerable<IProductContent>>("featuredProducts");
+
+            if (featuredProducts == null || !featuredProducts.Any()) return empty;
+
+            return featuredProducts.Take(3).Select(x => x.AsProductListItem());
+        }
+    }
+}
