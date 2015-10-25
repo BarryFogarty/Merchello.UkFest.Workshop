09a6840edbeb883ed3f5b1ceca6230a40f059420
Barry Fogarty <code@barryfogarty.com>
2015-10-25 19:57:56 +0000

    Wire up the TextPage

---
+++ Merchello.UkFest.Web/Models/TextPage.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+
+namespace Merchello.UkFest.Web.Models
+{
+    public class TextPage
+    {
+        /// <summary>
+        /// Gets or sets the page title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the body text
+        /// </summary>
+        public IHtmlString Text { get; set; }
+
+    }
+}
