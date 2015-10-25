5b76fd5644549c96ed5ef211ac41ac5a7a248e08
Barry Fogarty <code@barryfogarty.com>
2015-10-25 20:02:50 +0000

    Wire up header panel (basic)

---
+++ Merchello.UkFest.Web/Models/Header.cs
@@ -0,0 +1,17 @@
+﻿using System.Web;
+
+namespace Merchello.UkFest.Web.Models
+{
+    public class Header
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        public string HeadTitle { get; set; }
+
+        /// <summary>
+        /// Gets or sets the brief.
+        /// </summary>
+        public IHtmlString HeadDescription { get; set; }
+    }
+}
