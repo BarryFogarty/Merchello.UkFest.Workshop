6d41941a6ee17e0264fb6a450a1ca78a1c532c9a
Barry Fogarty <code@barryfogarty.com>
2015-10-26 17:32:06 +0000

    Product Listing partial

---
+++ Merchello.UkFest.Web/Models/ProductListing.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Merchello.UkFest.Web.Ditto.ValueResolvers;
+using Our.Umbraco.Ditto;
+
+namespace Merchello.UkFest.Web.Models
+{
+    public class ProductListing
+    {
+        /// <summary>
+        /// Gets or sets the products.
+        /// </summary>
+        [DittoValueResolver(typeof(ProductListItemsValueResolver))]
+        public virtual IEnumerable<ProductListItem> Products { get; set; }
+    }
+}
