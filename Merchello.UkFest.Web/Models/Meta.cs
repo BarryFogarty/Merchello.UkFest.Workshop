bd553966700f3c3396441475a8928017b0a8b950
Barry Fogarty <code@barryfogarty.com>
2015-10-26 18:57:59 +0000

    Meta tags on base layout

---
+++ Merchello.UkFest.Web/Models/Meta.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Web;
+using Merchello.UkFest.Web.Ditto.TypeConverters;
+using Our.Umbraco.Ditto;
+
+namespace Merchello.UkFest.Web.Models
+{
+    [UmbracoProperties(Prefix = "Head")]
+    public class Meta
+    {
+        /// <summary>
+        /// Gets or sets the title.
+        /// </summary>
+        [UmbracoProperty("HeadTitle", "Name")]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets the brief.
+        /// </summary>
+        [TypeConverter(typeof(MetaDescriptionConverter))]
+        public IHtmlString Description { get; set; }
+    }
+}
